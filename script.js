5f4658d0896e26c16905af9a1a7bf35a75fce984
Chinmay Datt D <chinmaydatt003@gmail.com>
2023-11-15 13:34:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function toCase(text) {
   // write your code here
+	if(text==""){
+		return "-";
+	}
+	let te=text.toLowerCase() + '-' + text.toUpperCase();
+	return te;
 }
 
 // DO not change the code below
 
-const text = prompt("Enter text:");
-alert(toCase(text));
+//const text = prompt("Enter text:");
+//alert(toCase(text));
